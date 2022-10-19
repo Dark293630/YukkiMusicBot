a68e6265f2666b8cc3552cf56ebfaf4ec1ed0149
Dark293630 <116166799+Dark293630@users.noreply.github.com>
2022-10-19 17:20:07 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -40,7 +40,7 @@ SONG_DOWNLOAD_DURATION = int(
 LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1001797910328"))
 
 # A name for your Music bot.
-MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "【 🇩‌𝐀𝐑𝐊 🇩‌𝐄𝐕𝐈𝐋 🇲​𝐔𝐒𝐈𝐂🇧​​​​​𝐎𝐓 】▄︻┻═┳一")
+MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "DarkDevilMusicBot")
 
 # Your User ID.
 OWNER_ID = list(
