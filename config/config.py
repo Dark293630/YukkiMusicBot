4cbda90954fb5622e30410266c974f89a8c508f9
Dark293630 <116166799+Dark293630@users.noreply.github.com>
2023-01-14 19:19:14 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -28,12 +28,12 @@ MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://Devarora0987:#Dev12345@clus
 
 # Custom max audio(music) duration for voice chat. set DURATION_LIMIT in variables with your own time(mins), Default to 60 mins.
 DURATION_LIMIT_MIN = int(
-    getenv("DURATION_LIMIT", "300")
+    getenv("DURATION_LIMIT", "3000")
 )  # Remember to give value in Minutes
 
 # Duration Limit for downloading Songs in MP3 or MP4 format from bot
 SONG_DOWNLOAD_DURATION = int(
-    getenv("SONG_DOWNLOAD_DURATION_LIMIT", "180")
+    getenv("SONG_DOWNLOAD_DURATION_LIMIT", "3000")
 )  # Remember to give value in Minutes
 
 # You'll need a Private Group ID for this.
